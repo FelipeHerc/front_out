8d53e75dd1994d86542d512f1bc3fd692d6b8bc1
FelipeHerc <hercrof@gmail.com>
2019-11-07 01:57:30 -0300

    insere meme em notfound

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+import nope from './nope.jpg';
+
+const ErrorBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 20px 10vw 20px 10vw;
+`
+const ErrorMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+`
 
 export const NotFound = () => {
   return (
-    <container>
-      <h1>404</h1>
-      <p>Sorry, the page you requested could not be found.</p>
-    </container>
+    <ErrorBox>
+      <img src={nope} alt="noooooooope" width="428" height="612"/>
+      <ErrorMessage>
+        <h1>404</h1>
+        <p>Sorry, the page you requested could not be found.</p>
+      </ErrorMessage>
+    </ErrorBox>
   );
 };
